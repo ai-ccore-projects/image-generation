aeae59513b9adcde2a58a8016bbc83aeec9aae02
Vijayaragupathy Vijayaragunathapandian <vvijayaragunathapa@unomaha.edu>
2025-06-26 19:49:44 -0500

    1

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,20 +1,22 @@
 'use client'
 
 import * as React from 'react'
-import {
-  ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
-} from 'next-themes'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import { type ThemeProviderProps } from 'next-themes/dist/types'
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
-    <NextThemesProvider 
-      {...props}
-      // Prevent hydration mismatch by ensuring theme is available immediately
-      storageKey="aiccore-theme"
-      suppressHydrationWarning
-    >
-      {children}
-    </NextThemesProvider>
+    <div suppressHydrationWarning>
+      <NextThemesProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+        storageKey="app-theme"
+        {...props}
+      >
+        {children}
+      </NextThemesProvider>
+    </div>
   )
 }
